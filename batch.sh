f6a09c4809549f3b3d108d9f2591e979ec4d3e6f
DinosaurMauricio <mauricio.eduardo.nr@gmail.com>
2024-05-28 22:34:36 +0200

    deleted valgrind from batch as i wont use it and other params that are set now on compile time

--- batch.sh
+++ batch.sh
@@ -12,18 +12,7 @@
 if [ "$#" -eq 2 ]; then
     srun ./bin/gpu_transpose "$1" "$2"
     exit 1
-elif [ "$#" -eq 3 ]; then 
-    if [[ "$3" == "--valgrind" ]]; then
-        srun valgrind --tool=cachegrind ./bin/gpu_transpose "$1" "$2" "$3"
-        exit 1
-    else 
-        srun ./bin/gpu_transpose "$1" "$2" "$3"
-        exit 1
-    fi
-elif [ "$#" -eq 4 ]; then 
-    srun valgrind --tool=cachegrind ./bin/gpu_transpose "$1" "$2" "$3" "$4"
-    exit 1
 else 
-    echo "Usage: $0 arg1 arg2 arg3 [--valgrind] or $0 arg1 arg2 [--valgrind] "
+    echo "Usage: $0 arg1 arg2"
     exit 1
 fi
